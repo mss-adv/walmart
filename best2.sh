36c5611b780828aa4985239a7e60fc9d52691e55
hi <kiran07kiranb@gmail.com>
2022-09-07 13:46:56 +0000

    succefull reverted by commaned git revert with code
    Revert "best2.shupdated by commend"
    
    This reverts commit ce36a922bb6b275a86ed0b4edfb4a6dfcb985ca1.

--- best2.sh
+++ best2.sh
@@ -1,7 +1,1 @@
- 
-
-
-
-hi we just checking the revert commend is working or not 
-
-hi this the test case for the git command to edit push and pull and add and remove etc
+ hi this the test case for the git command to edit push and pull and add and remove etc
